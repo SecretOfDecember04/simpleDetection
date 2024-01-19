90754275b97b3dc4157a88f09586b8904057cd87
SecretOfDecember04 <lin.3881.buckeye@gmail.com>
2024-01-19 17:36:07 -0500

    created the additional method called handleTap.

--- simpleDetection/ViewController.swift
+++ simpleDetection/ViewController.swift
@@ -16,7 +16,27 @@ class ViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // fire off plane detection
+        startPlaneDetection()
+        
+        // 2D point
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
+        
+        
     }
+    
+    @objc
+    func handleTap(recognizer: UITapGestureRecognizer){
+        //Touch location
+        let tapLocation = recognizer.location(in: arView)
+        
+        
+    }
+    
+    
+    
+    
+    
     func startPlaneDetection(){
         
         arView.automaticallyConfigureSession = true
@@ -24,6 +44,9 @@ class ViewController: UIViewController{
         let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = [.horizontal]
         configuration.environmentTexturing = .automatic
+        
+        
+        arView.session.run(configuration)
         
     }
 
